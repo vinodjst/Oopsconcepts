302ce3290ef6098593fee15f43c1b498748dc6a8
unknown <vinnu81993@gmail.com>
2025-05-02 07:26:31 +0530

    removed unwanted code

--- OOPS/src/Main.java
+++ OOPS/src/Main.java
@@ -2,10 +2,10 @@ import com.oops.inheritance.Bus;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//GIT CHECk
+//
 public class Main {
     public static void main(String[] args) {
-
-
 
 
     }
